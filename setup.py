397497f7a207dfb55ae24fa66b87f3845952fc1e
Wolfgang Schnerring <wosc@wosc.de>
2015-01-10 10:41:33 +0100

    Create skeleton

---
+++ setup.py
@@ -0,0 +1,43 @@
+"""Provides an easy way to configure git username on a per repository basis.
+"""
+from setuptools import setup, find_packages
+import glob
+import os.path
+
+
+def project_path(*names):
+    return os.path.join(os.path.dirname(__file__), *names)
+
+
+setup(
+    name='ws.gitpersona',
+    version='1.0.dev0',
+
+    install_requires=[
+        'setuptools',
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'git-persona = ws.gitpersona.persona:main'
+        ],
+    },
+
+    author='Wolfgang Schnerring <wosc@wosc.de>',
+    author_email='wosc@wosc.de',
+    license='ZPL 2.1',
+    url='https://github.com/wosc/git-persona/',
+
+    description=__doc__.strip(),
+    long_description='\n\n'.join(open(project_path(name)).read() for name in (
+        'README.txt',
+        'CHANGES.txt',
+    )),
+
+    namespace_packages=['ws'],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    data_files=[('', glob.glob(project_path('*.txt')))],
+    zip_safe=False,
+)
---
+++ src/ws/gitpersona/persona.py
@@ -0,0 +1,2 @@
+def main(argv=None):
+    pass
