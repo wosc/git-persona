58fd43f6788b14c3b74450afefa04bb9e7037560
Wolfgang Schnerring <wosc@wosc.de>
2015-01-10 11:28:12 +0100

    Implement listing personas

--- src/ws/gitpersona/persona.py
+++ src/ws/gitpersona/persona.py
@@ -1,2 +1,45 @@
+import argparse
+import re
+import subprocess
+
+
 def main(argv=None):
-    pass
+    parser = argparse.ArgumentParser(
+        description='Specify or list repository-local persona(s).')
+    parser.add_argument('--name', '-n', help='persona name')
+    options = parser.parse_args(argv)
+    personas = list_personas()
+    if options.name:
+        pass
+    else:
+        print('Known personas:')
+        for name, persona in personas.items():
+            print('  {persona} {name} <{email}>'.format(
+                persona=name.ljust(12), **persona))
+        print('Current username:')
+        print('  {name} <{email}>'.format(
+            name=cmd('git config user.name').strip(),
+            email=cmd('git config user.email').strip()))
+
+
+CONFIG_PERSONA = re.compile('^persona\\.(.*?) ([^<]*) <(.*?)>$')
+
+
+def list_personas():
+    result = {}
+    config = cmd('git config --global --get-regex ^persona\\.')
+    for line in config.splitlines():
+        match = CONFIG_PERSONA.search(line)
+        if not match:
+            continue
+        result[match.group(1)] = {
+            'name': match.group(2), 'email': match.group(3)}
+    return result
+
+
+def cmd(cmd):
+    process = subprocess.Popen(
+        cmd, shell=True,
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    return stdout
---
+++ src/ws/gitpersona/tests/test_persona.py
@@ -0,0 +1,36 @@
+from ws.gitpersona.persona import cmd
+import os
+import pytest
+import ws.gitpersona.persona
+
+
+@pytest.fixture(scope='session')
+def personas(request):
+    cmd('git config --global persona.testhome'
+        ' "First Last <home@example.com>"')
+    cmd('git config --global persona.testwork'
+        ' "First Last <work@example.com>"')
+
+    def teardown():
+        cmd('git config --global --unset persona.testhome')
+        cmd('git config --global --unset persona.testwork')
+    request.addfinalizer(teardown)
+
+
+@pytest.fixture
+def repository(request, tmpdir):
+    cwd = os.getcwd()
+    request.addfinalizer(lambda: os.chdir(cwd))
+    os.chdir(str(tmpdir))
+    # Usually one would put the personas in the global configuration,
+    # but for the tests we simply use the repository config instead.
+    cmd('git init;'
+        .format(dir=str(tmpdir)))
+
+
+def test_list_personas(personas):
+    personas = ws.gitpersona.persona.list_personas()
+    assert personas['testhome'] == {
+        'name': 'First Last', 'email': 'home@example.com'}
+    assert personas['testwork'] == {
+        'name': 'First Last', 'email': 'work@example.com'}
